b6d739cefdf536525863dc0961112ff7abecec00
deivid frechou <58941665+deividfrechou@users.noreply.github.com>
2022-10-06 13:27:38 -0300

    Modelos diferentes de incluir e recuperar dados

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ function salvar(){
 function carregar(){
     document.getElementById("dado").value = localStorage.info;
 }
+
+function salvar2(){
+    localStorage.setItem("nome",document.getElementById("dado").value);
+}
+
+function carregar2(){
+    document.getElementById("texto").innerHTML = localStorage.getItem("nome");
+}
+
 function apagar(){
     localStorage.removeItem("info");
+    localStorage.removeItem("nome");
 }
